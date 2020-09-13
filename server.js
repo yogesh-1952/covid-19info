4b7cc14b2e5108dc25308471e9e5b4602d967749
kartik sood <kartiks2k@gmail.com>
2020-09-13 14:04:32 +0530

    added backend

--- server.js
+++ server.js
@@ -16,20 +16,48 @@ app.get("/", (req, res) => {
 
 app.get("/helpline", (request, response) => {
 
-  var req=unirest("GET","https://api.rootnet.in/covid19-in/contacts");
-  req.end(function(res) {
-    if (res.error) throw new Error(res.error);
-    console.log(success);
+  unirest.get("https://api.rootnet.in/covid19-in/contacts")
+  .then((res)=>{
+    var contactObject = res.body;
+    var newContact = Object.values(contactObject.data.contacts.regional);
+    console.log(newContact);
+    response.render("helpline",{contactArray:newContact,primaryContacts:contactObject.data.contacts.primary});
   });
-      //const contactInfo = JSON.parse(data);
-      /* const arrayInfo = contactInfo.data.contacts.regional;
-      for (var i = 0; i < arrayInfo.length; i++) {
-        res.write(arrayInfo[i].loc + " " + arrayInfo[i].number);
-      }
-      console.log(arrayInfo[0]);
-      res.send(); */
-      response.send("Hello");
+});
+
+app.get("/notifications", (request, response) => {
+
+  unirest.get("https://api.rootnet.in/covid19-in/notifications")
+  .then((res)=>{
+    var notificationsObject = res.body;
+    var newNotifications = Object.values(notificationsObject.data.notifications);
+    response.render("notifications",{notificationsArray:newNotifications});
+  });
+});
+
+app.get('/hospitals/beds',function(request,response)
+{
+  unirest.get("https://api.rootnet.in/covid19-in/hospitals/beds").then
+    (function(res)
+    {
+        var hospitalObj=res.body;
+        var hregionArr=hospitalObj.data.regional;
+        response.render("beds",{hregionArr:hregionArr});
     });
+
+});
+
+app.get('/hospitals/colleges',function(request,response)
+{
+    unirest.get("https://api.rootnet.in/covid19-in/hospitals/medical-colleges").then
+    (function(res)
+    {
+        var medCollege=res.body;
+        var medCollegeArr=medCollege.data.medicalColleges;
+
+        response.render("colleges",{medCollegeArr:medCollegeArr})
+    })
+});
 
 app.listen(port, () => {
   console.log("Port 3000 started");
